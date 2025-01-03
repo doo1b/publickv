9de1b54b9bb97e30cdbcb8f8b3ceb2e796106bfa
doo1b <tngml928@gmail.com>
2025-01-03 15:58:05 +0900

    fix: input 컴포넌트 props 타입 해결

--- src/app/expense/components/Input.tsx
+++ src/app/expense/components/Input.tsx
@@ -1,18 +1,24 @@
 const Input = ({
   title,
   error,
+  value,
+  onChange,
   ...rest
 }: {
   title: string;
   error?: string;
+  value?: string | number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
     <label className="relative flex w-full items-center justify-between">
       {title}
       <input
         {...rest}
+        value={value}
+        onChange={onChange}
         className="body-14-r rounded-md border-[1px] border-secondary-800 px-2 py-1 text-secondary-800 focus:border-secondary-800 focus:outline-none"
-      ></input>
+      />
       {error && (
         <p className="caption absolute -bottom-4 right-1 font-semibold text-red-500">
           {error}
